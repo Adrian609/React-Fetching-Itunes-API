fa58504c2a45e4b4b1a1c8f4d43e8f67826c0c82
Adrian <Rodriguez.Adrian609@gmail.com>
2018-06-13 15:57:25 -0400

    Fetch connecting with API, Returning properties: Artist, Album, Album preview

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FetchAPI from './FetchAPI';
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Fetching Itunes API Example</h1>
         </header>
+       
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <FetchAPI />
+   
       </div>
     );
   }
